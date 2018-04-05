f9e3b55856c58e29b88a4928ba689406af3a8e15
albladh <albladh@kth.se>
2018-04-05 20:13:59 +0200

    Add custom theme

--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  cyan700,
+  grey600,
+  pinkA100, pinkA200, pinkA400,
+  fullWhite,
+} from 'material-ui/styles/colors';
+import {fade} from 'material-ui/utils/colorManipulator';
+
+const muiTheme = getMuiTheme({
+  palette:{
+    primary1Color: "#8c5ce4",
+    Primary2Color: "#6d4bac",
+    Primary3Color: "#6d4bac",
+    Accent1Color: "#FCE95F",
+    Accent2Color: "#E3CE39",
+    Accent3Color: "#FFF083",
+    textColor: fullWhite,
+   secondaryTextColor: fade(fullWhite, 0.7),
+   alternateTextColor: '#303030',
+   canvasColor: '#303030',
+   borderColor: fade(fullWhite, 0.3),
+   disabledColor: fade(fullWhite, 0.3),
+   pickerHeaderColor: fade(fullWhite, 0.12),
+  clockCircleColor: fade(fullWhite, 0.12),
+  }
+});
+
+
 
 ReactDOM.render((
     <BrowserRouter>
-      <App />
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <App />
+      </MuiThemeProvider>
     </BrowserRouter>
 ),document.getElementById('root'));
 registerServiceWorker();
+
+export default muiTheme;
