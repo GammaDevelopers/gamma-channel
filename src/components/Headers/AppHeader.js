ccda806a9d14a98e06fa3c9f2785bb96c9d9f38c
Victoria Hedenmalm <viched@kth.se>
2018-04-06 16:54:45 +0200

    Rename BoardHeader -> Header, Appheader to own compontent

---
+++ src/components/Headers/AppHeader.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../../index.css';
+import Paper from 'material-ui/Paper';
+import './BoardHeader.css';
+import HeaderLinks from './HeaderLinks';
+import logo from '../../images/logo.png';
+
+
+class AppHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { }
+  }
+  render() {
+    return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to Gamma Channel!</h1>
+      </header>
+    );
+  }
+}
+
+export default AppHeader;
