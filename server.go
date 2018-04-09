90cbad4c703a02a98dfa8cbe754b8f45f0c22ed3
Alexander Westberg-Bladh <alexander.bladh@gmail.com>
2018-04-09 21:57:55 +0200

    Add boards api endpoint

--- server.go
+++ server.go
@@ -1,19 +1,63 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "encoding/json"
-    "database/sql"
-    "github.com/lib/pq"
+	"database/sql"
+	"encoding/json"
+	//	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
 )
 
+type Board struct {
+	Name         string
+	Abbreviation string
+	Rules        string
+}
+
+func getDB() *sql.DB {
+
+	connStr := "user=testuser password=qwerty dbname=gamma sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return db
+}
+
+func boards(w http.ResponseWriter, r *http.Request) {
+	db := getDB()
+	rows, err := db.Query(`SELECT name, abbreviation FROM BOARDS`)
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer rows.Close()
+	var boards []Board
+	for rows.Next() {
+		var board Board
+		err := rows.Scan(&board.Name, &board.Abbreviation)
+		if err != nil {
+			log.Println(err)
+		} else {
+			boards = append(boards, board)
+		}
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Println(err)
+	}
+
+	json.NewEncoder(w).Encode(boards)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/api/boards", boards)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
