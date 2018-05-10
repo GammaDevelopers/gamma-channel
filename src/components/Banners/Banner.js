afb0b326bd97658259ac852e5b4f7d5c70fa68db
Alexander Bladh <albladh@kth.se>
2018-05-10 14:48:36 +0200

    Complete dynamic banner fix: #21

--- src/components/Banners/Banner.js
+++ src/components/Banners/Banner.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import '../../index.css';
 import './Banner.css';
-import testBanner from '../../images/testBanner.png';
+import {modelInstance} from '../../data/Model';
 
 class Banner extends Component {
   constructor(props) {
     super(props);
-    this.state = { }
+    this.state = { 
+      image: null,
+      description: null,
+    }
   }
+
+  componentDidMount(){
+    modelInstance.getHeaderImage().then((banner) => { 
+      this.setState({'image':banner.image,
+      'description': `Banner Creator: ${banner.submitter} 
+      id: ${banner.id} tickets: ${banner.weight}`})
+  })
+  }
+
   render() {
-    return (
-      <div>
-        <img id="banner" src={testBanner} className="testBanner" alt="testBanner" />
-      </div>
-    );
+    if (this.state.image !== null ) {
+      return (
+        <div>
+          <img class="banner" src={this.state.image} 
+                              alt={this.state.description} title={this.state.description} />
+            
+        </div>
+      );
+    }else{
+      return (<div></div>)
+    }
   }
 }
 
