3ff665c5435491ec5089bdb2e6dbae7211739961
Victoria Hedenmalm <viched@kth.se>
2018-05-18 23:00:26 +0200

    Improve search bar visibility

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {default as SearchBarUi} from 'material-ui-search-bar';
+import SearchIcon from 'material-ui/svg-icons/action/search';
 import './SearchBar.css';
 
 
@@ -25,7 +26,13 @@ export default class SearchBar extends React.Component {
         ref={input => this.search = input}
         onChange={(value) => this.onSearchChange(value)}
         onRequestSearch={() => this.pass}
-        hintText = {this.getHintText()}
+        hintText={this.getHintText()}
+        hintStyle={{color:"rgba(255, 255, 255, 0.54)"}}
+        searchIcon={<SearchIcon color={"white"}/>}
+        style = {{
+          borderRadius: "8px",
+          backgroundColor: "#222244"
+        }}
         />
       </div>
     );
